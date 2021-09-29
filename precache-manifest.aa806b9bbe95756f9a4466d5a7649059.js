a515ca5a72b23f116778ce3832b982afb15be069
James Park <james.park.victoria@gmail.com>
2021-09-29 16:19:03 -0700

    Updates

---
+++ precache-manifest.aa806b9bbe95756f9a4466d5a7649059.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe649d24f0bb4ca06840325e01bae4d",
+    "url": "/csc-375-website/index.html"
+  },
+  {
+    "revision": "bce97a628b7691aebee9",
+    "url": "/csc-375-website/static/css/main.e1f6fd3d.chunk.css"
+  },
+  {
+    "revision": "c2192de752ce451bdf08",
+    "url": "/csc-375-website/static/js/2.7df14fa6.chunk.js"
+  },
+  {
+    "revision": "33834944fef3642f8d6e8eb1cb24e412",
+    "url": "/csc-375-website/static/js/2.7df14fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce97a628b7691aebee9",
+    "url": "/csc-375-website/static/js/main.01ee86b2.chunk.js"
+  },
+  {
+    "revision": "4d2b4828e03fcb29315f",
+    "url": "/csc-375-website/static/js/runtime-main.1828dc28.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/csc-375-website/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "bc2294f981e4bab79922da8bac2f66b5",
+    "url": "/csc-375-website/static/media/features-split-image-01.bc2294f9.png"
+  },
+  {
+    "revision": "6bc0c7ac99fcf1ae260d23d90c4e7039",
+    "url": "/csc-375-website/static/media/features-split-image-02.6bc0c7ac.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/csc-375-website/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/csc-375-website/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "d54a74a9b163ff8a731c9502a12f2b5f",
+    "url": "/csc-375-website/static/media/initial-image.d54a74a9.jpg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/csc-375-website/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "0904db246538cbd1504034209756c23c",
+    "url": "/csc-375-website/static/media/profile2.0904db24.jpg"
+  },
+  {
+    "revision": "3ffc8494bcd76cec53f0a79eb3a4b4f6",
+    "url": "/csc-375-website/static/media/profile6.3ffc8494.jpg"
+  },
+  {
+    "revision": "d26aeea251639acab5763a2a3e0c1782",
+    "url": "/csc-375-website/static/media/rfp.d26aeea2.pdf"
+  }
+]);
